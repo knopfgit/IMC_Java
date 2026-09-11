0176566dc08ac5e6f0c321ebc6fe06d4e3367890
Knopfgit <githubknopf@gmail.com>
2026-09-11 12:56:56 -0300

    Adiciona programa de calculo do IMC e melhora na formatacao

--- IMC.java
+++ IMC.java
@@ -9,3 +9,64 @@ double peso = Double.parseDouble(IO.readln("Informe seu peso em quilos: "));
 
 double imc = peso / (altura * altura);
 
+String classificacao = "";
+
+switch (genero) {
+    case 'M':
+        if (imc >= 40) {
+            classificacao = "Obesidade Mórbida";
+        } else if (imc >= 30) {
+            classificacao = "Obesidade Moderada";
+        } else if (imc >= 25) {
+            classificacao = "Obesidade Leve";
+        } else if (imc >= 20) {
+            classificacao = "Normal";
+        } else {
+            classificacao = "Abaixo do Normal";
+        }
+        break;
+
+    case 'F':
+        if (imc >= 39) {
+            classificacao = "Obesidade Mórbida";
+        } else if (imc >= 29) {
+            classificacao = "Obesidade Moderada";
+        } else if (imc >= 24) {
+            classificacao = "Obesidade Leve";
+        } else if (imc >= 19) {
+            classificacao = "Normal";
+        } else {
+            classificacao = "Abaixo do Normal";
+        }
+        break;
+
+    case 'N':
+        if (imc >= 39) {
+            classificacao = "Obesidade Mórbida";
+        } else if (imc >= 29) {
+            classificacao = "Obesidade Moderada";
+        } else if (imc >= 24) {
+            classificacao = "Obesidade Leve";
+        } else if (imc >= 19) {
+            classificacao = "Normal";
+        } else {
+            classificacao = "Abaixo do Normal";
+        }
+        break;
+
+    default:
+        IO.println("Gênero inválido.");
+        break;
+}
+
+if (genero == 'M' || genero == 'F' || genero == 'N') {
+    IO.println("");
+    IO.println("Nome: " + nome);
+    IO.println("Gênero: " + genero);
+    IO.println("IMC: " + imc);
+    IO.println("Classificação: " + classificacao);
+}
+
+IO.println("");
+IO.println("Atitus Educação - O lado certo da força!!!");
+```
